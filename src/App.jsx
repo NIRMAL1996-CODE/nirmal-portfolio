3c016f9c15cccc2204511e5681244bc420e60a97
Nirmal Jaiswal <nirmaljaiswal888@gmail.com>
2025-12-03 17:11:42 +0530

    portfolio

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+// src/App.jsx
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+import Hero from "./components/Hero";
+import About from "./components/About";
+import Projects from "./components/Projects";
+import Contact from "./components/Contact";
+
+const App = () => {
+  return (
+    <>
+      <Navbar />
+      <Hero/>
+      <About/>
+      <Projects/>
+      <Contact/>
+      <Footer/>
+    </>
+  );
+};
+
+export default App;
